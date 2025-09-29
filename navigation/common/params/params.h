8016015f0c4752dd2c5d34a69e7a2f4e041549e0
discountchubbs <alexgrant990@gmail.com>
2025-09-29 05:53:51 -0700

    Update params.h

--- navigation/common/params/params.h
+++ navigation/common/params/params.h
@@ -1,10 +1,10 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
-#include <map>
+#include <unordered_map>
 #include <string>
 
-const std::map<std::string, std::string> DEFAULT_PARAMS = {
+const std::unordered_map<std::string, std::string> DEFAULT_PARAMS = {
   {"MapboxSettings", ""},
   {"IsMetric", "false"},
   {"MapboxToken", ""}
