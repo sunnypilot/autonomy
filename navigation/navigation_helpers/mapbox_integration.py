b4bc505c9efaf5610c598c52f204a5983b48e7fa
discountchubbs <alexgrant990@gmail.com>
2025-10-12 13:13:27 -0700

    reduce some repetition

--- navigation/navigation_helpers/mapbox_integration.py
+++ navigation/navigation_helpers/mapbox_integration.py
@@ -2,7 +2,6 @@ from urllib.parse import quote
 import requests
 
 from common.params.params import Params
-from navigation.navd.helpers import Coordinate
 
 
 class MapboxIntegration:
@@ -18,7 +17,7 @@ class MapboxIntegration:
       self.nav_confirmed(postvars, current_lon, current_lat, bearing)
       return postvars, True
 
-    addr = postvars.get('place_name')
+    addr = postvars['place_name']
     if not addr:
       return postvars, False
 
@@ -46,24 +45,7 @@ class MapboxIntegration:
     token = self.get_public_token()
     route_data = self.generate_route(start_lon, start_lat, longitude, latitude, token, bearing)
     if route_data:
-      data['navData']['route'] = {
-        'steps': [
-          {
-            'maneuver': step['maneuver'],
-            'instruction': step['instruction'],
-            'distance': step['distance'],
-            'duration': step['duration'],
-            'location': {'latitude': step['location'].latitude, 'longitude': step['location'].longitude},
-            'modifier': step['modifier'],
-            'bannerInstructions': step['bannerInstructions'],
-          }
-          for step in route_data['steps']
-        ],
-        'totalDistance': route_data['total_distance'],
-        'totalDuration': route_data['total_duration'],
-        'geometry': [{'longitude': coord[0], 'latitude': coord[1]} for coord in route_data['geometry']],
-        'maxspeed': route_data['maxspeed'],
-      }
+      data['navData']['route'] = route_data
     self.params.put('MapboxSettings', data)
 
   def generate_route(self, start_lon, start_lat, end_lon, end_lat, token, bearing=None) -> dict | None:
@@ -76,36 +58,31 @@ class MapboxIntegration:
 
     response = requests.get(f'https://api.mapbox.com/directions/v5/mapbox/driving/{start_lon},{start_lat};{end_lon},{end_lat}', params=params, timeout=10)
     data = response.json() if response.status_code == 200 else {}
-    if data['code'] != 'Ok':  # status code 200 returns Ok, NoRoute, or NoSegment, we only want Ok responses
+    routes = data['routes'] if data else None
+    legs = routes[0]['legs'] if routes else None
+
+    if data.get('code') != 'Ok' or not routes or not legs:
       return None
 
-    routes = data.get('routes', [])
-    legs = routes[0].get('legs', []) if routes else []
-
-    if not routes or not legs:
-      return None
-
-    leg = legs[0]
     route = routes[0]
+    leg = legs[0]
+
     steps = [
-      {
-        'maneuver': step['maneuver']['type'],
-        'instruction': step['maneuver']['instruction'],
-        'distance': step['distance'],
-        'duration': step['duration'],
-        'location': Coordinate.from_mapbox_tuple(tuple(step['maneuver']['location'])),
-        'modifier': step['maneuver'].get('modifier', 'none'),
-        'bannerInstructions': step['bannerInstructions'],
-      }
-      for step in leg['steps']
-    ]
+      {'maneuver': step['maneuver']['type'],
+       'instruction': step['maneuver']['instruction'],
+       'distance': step['distance'],
+       'duration': step['duration'],
+       'location': {'longitude': step['maneuver']['location'][0], 'latitude': step['maneuver']['location'][1]},
+       'modifier': step['maneuver'].get('modifier', 'none'),
+       'bannerInstructions': step['bannerInstructions']}
+      for step in leg['steps']]
 
     maxspeed = [{'speed': item['speed'], 'unit': item['unit']} for item in leg['annotation']['maxspeed'] if 'speed' in item]
 
     return {
       'steps': steps,
-      'total_distance': route['distance'],
-      'total_duration': route['duration'],
-      'geometry': route['geometry']['coordinates'],
+      'totalDistance': route['distance'],
+      'totalDuration': route['duration'],
+      'geometry': [{'longitude': coord[0], 'latitude': coord[1]} for coord in route['geometry']['coordinates']],
       'maxspeed': maxspeed,
     }
